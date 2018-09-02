fab547d2fcfbb2d4fdf20d75d6419f6f54f208bf
Earthcomputer <burtonjae@hotmail.co.uk>
2018-09-03 00:21:55 +0100

    Fix MixinContainerBeacon failing to apply

--- src/main/java/net/earthcomputer/enchcrack/mixin/MixinContainerBeacon.java
+++ src/main/java/net/earthcomputer/enchcrack/mixin/MixinContainerBeacon.java
@@ -1,7 +1,6 @@
 package net.earthcomputer.enchcrack.mixin;
 
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -11,18 +10,14 @@ import net.minecraft.client.entity.EntityPlayerSP;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.inventory.Container;
 import net.minecraft.inventory.ContainerBeacon;
-import net.minecraft.inventory.Slot;
 
 @Mixin(ContainerBeacon.class)
 public abstract class MixinContainerBeacon extends Container {
 
-	@Shadow
-	private Slot beaconSlot;
-
 	@Inject(method = "onContainerClosed", at = @At("HEAD"))
 	public void onOnContainerClosed(EntityPlayer player, CallbackInfo ci) {
 		if (player instanceof EntityPlayerSP) {
-			if (beaconSlot.getStack().getCount() > 1) {
+			if (this.inventorySlots.get(0).getStack().getCount() > 1) {
 				EnchantmentCracker.dropItemCheck();
 			}
 		}
