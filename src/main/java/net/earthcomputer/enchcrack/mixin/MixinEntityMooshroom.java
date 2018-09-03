4f59dd5ece089392fcd4024d333750ad1c01379d
Earthcomputer <burtonjae@hotmail.co.uk>
2018-09-03 22:02:42 +0100

    Fix synthetic bridge crash (#2)

--- src/main/java/net/earthcomputer/enchcrack/mixin/MixinEntityMooshroom.java
+++ src/main/java/net/earthcomputer/enchcrack/mixin/MixinEntityMooshroom.java
@@ -6,23 +6,17 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import net.earthcomputer.enchcrack.EnchantmentCracker;
-import net.minecraft.entity.passive.EntityCow;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.passive.EntityMooshroom;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.EnumHand;
-import net.minecraft.world.World;
 
 @Mixin(EntityMooshroom.class)
-public abstract class MixinEntityMooshroom extends EntityCow {
-
-	public MixinEntityMooshroom(World world) {
-		super(world);
-	}
+public abstract class MixinEntityMooshroom {
 
 	@Inject(method = "processInteract", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/passive/EntityMooshroom;setDead()V"))
-	//@Inject(method = "processInteract", at = @At("HEAD"))
 	public void onProcessInteract(EntityPlayer player, EnumHand hand, CallbackInfoReturnable<Boolean> ci) {
-		if (world.isRemote) {
+		if (((Entity) (Object) this).world.isRemote) {
 			EnchantmentCracker.toolDamageCheck(player.getHeldItem(hand), 1);
 		}
 	}
