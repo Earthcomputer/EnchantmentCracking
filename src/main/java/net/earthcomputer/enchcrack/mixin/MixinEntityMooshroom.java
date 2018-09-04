7fa2f96e404930be7327553b36c472834cfc7030
Earthcomputer <burtonjae@hotmail.co.uk>
2018-09-04 16:36:18 +0100

    Fix Forge incompatibility

--- src/main/java/net/earthcomputer/enchcrack/mixin/MixinEntityMooshroom.java
+++ src/main/java/net/earthcomputer/enchcrack/mixin/MixinEntityMooshroom.java
@@ -7,17 +7,22 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import net.earthcomputer.enchcrack.EnchantmentCracker;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityAgeable;
 import net.minecraft.entity.passive.EntityMooshroom;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Items;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.EnumHand;
 
 @Mixin(EntityMooshroom.class)
 public abstract class MixinEntityMooshroom {
 
-	@Inject(method = "processInteract", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/passive/EntityMooshroom;setDead()V"))
+	@Inject(method = "processInteract", at = @At("HEAD") )
 	public void onProcessInteract(EntityPlayer player, EnumHand hand, CallbackInfoReturnable<Boolean> ci) {
 		if (((Entity) (Object) this).world.isRemote) {
-			EnchantmentCracker.toolDamageCheck(player.getHeldItem(hand), 1);
+			ItemStack heldItem = player.getHeldItem(hand);
+			if (heldItem.getItem() == Items.SHEARS && ((EntityAgeable) (Object) this).getGrowingAge() >= 0)
+				EnchantmentCracker.toolDamageCheck(heldItem, 1);
 		}
 	}
 
